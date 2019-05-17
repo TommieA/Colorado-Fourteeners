7296f72dd54ab0737a454d9cc59bd3476d6343e8
Tommie <tommiealcorn@gmail.com>
2019-05-17 12:35:17 -0600

    Fixed other typo on ReviewController

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next)=>{
     next();
 })
 
-const reviewController = require('./controllers/ReviewController');
+const reviewController = require('./Controllers/ReviewController');
 app.use('/reviews', ReviewController);
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
